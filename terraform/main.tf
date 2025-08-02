802d9aa21c93074465b55659f95aa3e51a6b1253
k-nagahama-ruby <k-nagahama@ruby-dev.jp>
2025-08-02 13:44:16 +0900

    feat : s3 backet for uplaod

---
+++ terraform/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"  # 東京リージョン
+}
+
+
+resource "aws_s3_bucket" "first_bucket" {
+
+  bucket = "orbie-helper-test-${formatdate("YYYYMMDD", timestamp())}"
+
+  tags = {
+    Name        = "Oruby Helper Test Bucket"
+    Environment = "development"
+    Purpose     = "Learning Terraform"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "first_bucket_pab" {
+  bucket = aws_s3_bucket.first_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+output "bucket_name" {
+  description = "作成されたS3バケットの名前"
+  value       = aws_s3_bucket.first_bucket.id
+}
